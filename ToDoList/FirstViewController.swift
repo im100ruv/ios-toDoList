d725190935e803e2a15928984a4830af4c970b56
im100ruv <karnasaurabh123@gmail.com>
2018-02-15 18:29:48 +0530

    initial commit

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -8,17 +8,60 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var taskTable: UITableView!
+    
+    var tasks = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let tasksObject = UserDefaults.standard.object(forKey: "tasks")
+        
+        
+        if let temptask = tasksObject as? [String] {
+            tasks = temptask
+        }
+        
+        taskTable.reloadData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+//    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+//        if editingStyle == .delete {
+//            taskTable.remove(at: indexPath.row)
+//            tableView.deleteRows(at: [indexPath], with: .fade)
+//        } else if editingStyle == .insert {
+//            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+//        }
+//    }
+
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tasks.remove(at: indexPath.row)
+            taskTable.deleteRows(at: [indexPath], with: .fade)
+            
+            taskTable.reloadData()
+            UserDefaults.standard.set(tasks, forKey: "tasks")
+        }
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = tasks[indexPath.row]
+        
+        return cell
+    }
+    
 
 
 }
